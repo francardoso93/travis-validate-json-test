02def6a0db829200ab92a7e452b4408530aa4b48
Francisco Cardoso <francardoso@outlook.com>
2018-09-24 21:23:06 -0300

    FirstCommit

--- test/test.js
+++ test/test.js
@@ -1,6 +1,19 @@
 var simple = require('../lib/script');
 var expect = require('expect.js');
 
+
+var fs = require('fs'),
+    path = require('path'),    
+    filePath = path.join("./", 'sera.json');
+    let parsedData = JSON.parse(fs.readFileSync(filePath,{encoding: 'utf-8'}));
+
+
+describe("JSON Schema", function() {
+  it("should have prop1 as 'provalue'", function(){
+    expect(parsedData.prop1).to.equal('provalue');
+  });
+});
+
 describe("simple", function(){
   it("should return true", function(){
     expect(simple.math(1)).to.equal(true);
